2197353f209865bd82fa34149f36e4cc85d03b9f
Omar Jaber <48085067+Ojay89@users.noreply.github.com>
2020-03-03 22:20:33 +0100

    .

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,12 +1,54 @@
-interface Person {
-    firstName: string;
-    lastName: string;
+
+let input: string = "";
+let textInput: HTMLInputElement = <HTMLInputElement>document.getElementById("textInput");
+let textOutput: HTMLDivElement = <HTMLDivElement>document.getElementById("textOutput");
+
+
+let showItButton: HTMLInputElement = <HTMLInputElement>document.getElementById("doItButton");
+showItButton.addEventListener("click", showWords)
+
+let changeToUpperCase: HTMLInputElement = <HTMLInputElement>document.getElementById("toUpperCase");
+changeToUpperCase.addEventListener("click", upper)
+
+let ChangeToLowerCase: HTMLInputElement = <HTMLInputElement>document.getElementById("toLowerCase");
+ChangeToLowerCase.addEventListener("click", lower)
+
+// let doItButton: HTMLInputElement = <HTMLInputElement>document.getElementById("doItButton");
+// doItButton.addEventListener("click", SaveWords)
+
+function showWords(): void {
+    
+    let userinput: string = textInput.value
+    let input = userinput;
+
+    if (input.length > 0) {
+    textOutput.innerHTML = String(input);
+    }
+    else {
+    textOutput.innerHTML = "<i>Empty<i>";
+    }
 }
 
-function greeter(person: Person): string {
-    return "Hello, " + person.firstName + " " + person.lastName;
-}
-let user: Person = { firstName: "John", lastName: "Doe" };
 
-let element: HTMLDivElement = <HTMLDivElement> document.getElementById("content");
-element.innerHTML = greeter(user);
+function upper(): void {
+    let userinput: string = textInput.value
+    let input = userinput;
+
+    if(changeToUpperCase) {
+    String (input).toUpperCase;
+    }
+}
+
+function lower(): void {
+    
+    
+}
+
+// function SaveWords(): void {
+//     let textInput: HTMLInputElement = <HTMLInputElement>document.getElementById("textInput");
+//     let userinput: string = textInput.value;
+//     words.push(userinput);
+// }
+
+
+
